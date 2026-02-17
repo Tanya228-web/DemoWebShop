5919ef54ca24e11a3972a582157d0990803ba94d
Tanya228-web <tanyasinghsikarwar1@gmail.com>
2026-02-17 18:51:55 +0530

    changes in login

--- src/test/java/Test/Login/LoginTest.java
+++ src/test/java/Test/Login/LoginTest.java
@@ -18,18 +18,18 @@ public class LoginTest extends BaseTest {
     }
 
 
-    @DataProvider(name = "verifyLogin")
+    @DataProvider(name = "verifyLoginData")
     public Object [][] verifyLoginData(){
 
         return new Object[][]{
-                {"Radhe1234567@gmail.com","123456","WrongEmail"},
-                {"tannu1@gmail.com","1237890","WrongPassword"},
-                {"","Empty"}
+                {"Radhe1234567@gmail.com","123456","checkErrorMsgBySendingWrongEmail"},
+                {"tannu1@gmail.com","1237890","checkErrorMsgBySendingWrongPassword"},
+                {"","","EmptyUserDetails"}
         };
     }
 
     @Test (dataProvider = "verifyLoginData")
-    public void verifyLogin(String email, String password, String validation){
+    public void verifyUserDetails(String email, String password, String validation){
         var LoginPage = homepage.goToLoginPage();
         LoginPage.userDetails(email, password);
         LoginPage.clickLoginBtn();
@@ -45,7 +45,7 @@ public class LoginTest extends BaseTest {
                 Assert.assertEquals(actualResult,"The credentials provided are incorrect");
                 break;
 
-            case "":
+            case "EmptyUserDetails":
                 actualResult = LoginPage.getErrorMsgWithoutSendingUserDetails();
                 Assert.assertEquals(actualResult,"No customer account found");
                 break;
